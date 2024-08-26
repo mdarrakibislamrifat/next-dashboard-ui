63d4e9cd1e123e4754ee58d9b7325db0eb0a0c3f
mdarrakibislamrifat <rohanrifat43@gmail.com>
2024-08-26 13:33:31 +0600

    Menu responsive

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -18,7 +19,10 @@ export default function DashboardLayout({
             <Menu/>
           </div>
           {/* Right */}
-          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] ">r</div>
+          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-x-scroll">
+            <Navbar/>
+            {children}
+          </div>
           </div>
       
     );

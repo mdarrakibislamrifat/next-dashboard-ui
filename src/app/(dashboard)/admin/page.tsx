38e5a551d913655f5d2622983ded8d023f07d45a
mdarrakibislamrifat <rohanrifat43@gmail.com>
2024-08-27 21:46:22 +0600

    Admin Dashboard design Done

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Usercard from "@/components/Usercard";
+
 
 const Page = () => {
   return (
-    <div>
-      Admin Page
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:2/3">
+        {/* USER cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <Usercard type="Student"/>
+          <Usercard type="Teacher"/>
+          <Usercard type="Parent"/>
+          <Usercard type="Staff"/>
+        </div>
+      </div>
+
+      {/*RIGHT  */}
+      <div className="w-full lg:1/3">
+
+      </div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        rifatSky: "#C3EBFA",
+        rifatSkyLite: "#EDF9FD",
+        rifatPurple: "#CFCEFF",
+        rifatPurpleLight: "#F1F0FF",
+        rifatYellow: "#FAE27C",
+        rifatYellowLight: "#FEFCE8",
+      },
     },
   },
   plugins: [],

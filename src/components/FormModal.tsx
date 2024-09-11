04523799d0a28c10e75ac1f8c1f5600d8a4bc7ae
mdarrakibislamrifat <rohanrifat43@gmail.com>
2024-09-11 22:52:30 +0600

    React hook form and zod validation intilize and setup

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -35,6 +35,21 @@ const FormModal = ({
       : "bg-rifatPurple";
   const [open, setOpen] = useState(false);
 
+  const Form = () => {
+    return type === "delete" && id ? (
+      <form action="" className="p-4 flex flex-col gap-4">
+        <span className="text-center font-medium">
+          All data will be lost. Are you sure you want to delete this {table}?
+        </span>
+        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
+          Delete
+        </button>
+      </form>
+    ) : (
+      "Form not found!"
+    );
+  };
+
   return (
     <>
       <button
@@ -46,6 +61,7 @@ const FormModal = ({
       {open && (
         <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
           <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            <Form />
             <div
               className="absolute top-4 right-4 cursor-pointer"
               onClick={() => setOpen(false)}
---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long!" })
+    .max(20, { message: "Username must be at most 20 characters long!" }),
+  email: z.string().email({ message: "Inavlid emil address!" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long!" }),
+  firstName: z.string().min(1, { message: "First name is required!" }),
+  lastName: z.string().min(1, { message: "Last name is required!" }),
+  phone: z.string().min(1, { message: "Phone is required!" }),
+  address: z.string().min(1, { message: "Address name is required!" }),
+  birthday: z.date({ message: "Birthday is required!" }),
+  sex: z.enum(["male", "female"], { message: "Sex is required!" }),
+  img: z.instanceof(File, { message: "Image is required!" }),
+});
+
+const TeacherForm = ({
+  type,
+  data,
+}: {
+  type: "create" | "update";
+  data?: any;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+  return (
+    <form className="flex flex-col gap-8 ">
+      <h1 className="text-xl font-semibold">Create a new teacher</h1>
+      <span>Authentication Information</span>
+    </form>
+  );
+};
+
+export default TeacherForm;

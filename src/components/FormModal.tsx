ebdb415ebdcf968c0a9ea4fd988aa9a7d0f910dd
mdarrakibislamrifat <rohanrifat43@gmail.com>
2024-09-11 21:37:33 +0600

    Modal design almost done

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 
 const FormModal = ({
   table,
@@ -32,13 +33,28 @@ const FormModal = ({
       : type === "update"
       ? "bg-rifatSky"
       : "bg-rifatPurple";
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <button
         className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
       >
         <Image src={`/${type}.png`} alt="" width={16} height={16} />
       </button>
+      {open && (
+        <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+          <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            <div
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={() => setOpen(false)}
+            >
+              <Image src="/close.png" alt="" width={14} height={14} />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
